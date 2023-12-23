ea5fb8fa379c4568f820489bb6c36b55c1e96f7f
Fakhrirudin <76417345+Fakhrirudin@users.noreply.github.com>
2023-12-23 22:09:04 +0700

    Update page.tsx

--- src/app/(profile)/profile/page.tsx
+++ src/app/(profile)/profile/page.tsx
@@ -28,7 +28,6 @@ export default function ProfilePage() {
     }
 
     return (
-        <PageIllustration/>,
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
